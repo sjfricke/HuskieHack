05dbc2e86861e420c26d5686e93542805d1dd00a
Spencer <spencer@branch2.com>
2015-09-26 17:15:59 -0500

    SORRY FOR PARTY ROCKING

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var routes = require('./routes/index');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -20,6 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', routes);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
