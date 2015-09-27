e81481644c6d077b2a2eeba00feb9238d7ddae86
Spencer <spencer@branch2.com>
2015-09-27 00:27:58 -0500

    shit happening

--- public/javascripts/play.js
+++ public/javascripts/play.js
@@ -5,17 +5,54 @@
   .controller('playController', function($scope) {
       var vm = this;
 
-      
-      vm.test=test;
-      function test(){alert('TEST');}
+     var tipPosition;
       
 Leap.loop(function(frame){
 $scope.$apply(function() {
    
-    
+  
+   
+    var controller = new Leap.Controller();
+    controller.on('frame', function(frame){
+        if(frame.pointables.length > 0){
+            for(var i = 0; i < 5; i++){
+                if (frame.pointables[i].tipVelocity[1] < -300){
+                console.log(i);
+                }
+            }
+        }
+    });
+    controller.connect();
+//    if (frame.pointables.length > 0) {
+//      for (var i = 0; i < frame.pointables.length; i++) {
+//        tipPosition = frame.pointables[1].tipPosition;
+//        var leapCoordinates[i] = vectorToString(tipPosition,1);
+//                
+//        //Normalized coordinates
+//        var normalizedPosition = interactionBox.normalizePoint(tipPosition, true);
+//        var normalizedCoordinates[i] = vectorToString(normalizedPosition,4);  
+//      }
+//        //grabs pointer finger tip
+//        
+//        
+//        
+//        console.log(normalizedCoordinates[1]);
+//        console.log(normalizedCoordinates[6]);
+//    }
 
 });
 });
+      
+//turns vector into string 
+function vectorToString(vector, digits) {
+  if (typeof digits === "undefined") {
+    digits = 1;
+  }
+  return "(" + vector[0].toFixed(digits) + ", "
+             + vector[1].toFixed(digits) + ", "
+             + vector[2].toFixed(digits) + ")";
+};
+
 
 
   });
