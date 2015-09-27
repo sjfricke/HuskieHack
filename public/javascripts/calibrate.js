242d7790eb3c16e3ade50cdb55c7b37b37361e44
Spencer <spencer@branch2.com>
2015-09-26 20:54:17 -0500

    calibration

--- public/javascripts/calibrate.js
+++ public/javascripts/calibrate.js
@@ -8,13 +8,30 @@
 vm.caliCounter = 1;
       
 Leap.loop(function(frame){
-    $scope.$apply(function() {
+$scope.$apply(function() {
     var interactionBox = frame.interactionBox;
+    
+    //only grabs if hand shown
+    if(frame.pointables.length > 0){
+        //grabs pointer finger tip
+        var tipPosition = frame.pointables[1].tipPosition;
+                
+        //Normalized coordinates
+        var normalizedPosition = interactionBox.normalizePoint(tipPosition, true);
+        vm.normalizedCoordinates = vectorToString(normalizedPosition,4);
+
+        //Pixel coordinates in current window
+        var windowPosition = [normalizedPosition[0] * window.innerWidth, 
+                              window.innerHeight - (normalizedPosition[1] * window.innerHeight), 
+                              0];
+        vm.windowCoordinates = vectorToString(windowPosition, 0);    
+    }
     
 
 });
 });
 
+//turns vector into string 
 function vectorToString(vector, digits) {
   if (typeof digits === "undefined") {
     digits = 1;
@@ -24,11 +41,14 @@ function vectorToString(vector, digits) {
              + vector[2].toFixed(digits) + ")";
 };
 
+//waits for spacebar to be pressed
 window.addEventListener('keydown', function(event) {
     if(event.keyCode === 32) {
         calibrationCounter();
     }
 });
+
+//runs if calibration is called
 function calibrationCounter() {
    
     vm.caliCounter = vm.caliCounter + 1;
